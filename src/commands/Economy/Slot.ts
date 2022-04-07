e052322ec8409aaa9b8df3842def39f61d302cd3
DEVILL-MASCOT <102029388+DEVILL-MASCOT@users.noreply.github.com>
2022-04-07 10:14:11 +0530

    Update Slot.ts

--- src/commands/Economy/Slot.ts
+++ src/commands/Economy/Slot.ts
@@ -121,7 +121,7 @@ export default class Command extends BaseCommand {
       const text = `${head}\n\n${o}\n\n📈 You won *${gold} gold*.`;
       const buttonMessage: any = {
         contentText: `${text}`,
-        footerText: "🎇 Beyond 🎇",
+        footerText: "🎀𝓜𝓐𝓡𝓘𝓝𝓔🎀",
         buttons: buttons,
         headerType: 1,
       };
@@ -137,7 +137,7 @@ export default class Command extends BaseCommand {
       const text = `${head}\n\n${p}\n\n🎊 *Jackpot!* You won *${gold} gold*.`;
       const buttonMessage: any = {
         contentText: `${text}`,
-        footerText: "🎇 Beyond 🎇",
+        footerText: "🎀𝓜𝓐𝓡𝓘𝓝𝓔🎀",
         buttons: buttons,
         headerType: 1,
       };

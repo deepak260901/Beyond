08fa99161337ac910f3b62a60b9314352149b4e9
DEVILL-MASCOT <102029388+DEVILL-MASCOT@users.noreply.github.com>
2022-04-16 10:08:08 +0530

    Update Party.ts

--- src/commands/Pokemon/Party.ts
+++ src/commands/Pokemon/Party.ts
@@ -50,7 +50,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `${text}`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "MARIN💕",
       buttons: buttons,
       headerType: 1,
     };

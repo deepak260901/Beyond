58295e087da8780e4da43127434b30e7d3d89153
DEVILL-MASCOT <102029388+DEVILL-MASCOT@users.noreply.github.com>
2022-04-16 10:09:20 +0530

    Update Pokemons.ts

--- src/commands/Pokemon/Pokemons.ts
+++ src/commands/Pokemon/Pokemons.ts
@@ -52,7 +52,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `${text}`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "MARIN💕",
       buttons: buttons,
       headerType: 1,
     };

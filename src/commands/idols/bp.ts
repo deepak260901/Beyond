eb5f7bc10d23ec8e37d8d9306328d0e382416172
DEVILL-MASCOT <102029388+DEVILL-MASCOT@users.noreply.github.com>
2022-05-15 19:22:22 +0530

    Update bp.ts

--- src/commands/idols/bp.ts
+++ src/commands/idols/bp.ts
@@ -12,36 +12,29 @@ export default class Command extends BaseCommand {
 			command: "blackpink",
 			description: "sends pics of anime boy",
 			category: "idols",
-                        aliases: ["blp"],
-			usage: `${client.config.prefix}blackpink`,
+                        aliases: ["bp","blp"],
+			usage: `${client.config.prefix}blp`,
 			baseXp: 10,
 		});
 	}
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const images = [
-    "https://wallpapercave.com/w/wp8808959.jpg",
-    "https://wallpapercave.com/w/wp10472453.jpg",
-    "https://wallpapercave.com/w/wp10502274.jpg",
-    "https://wallpapercave.com/w/wp7850224.jpg",
-    "https://wallpapercave.com/w/wp10468512.jpg",
-    "https://wallpapercave.com/w/wp9832405.jpg",
-    "https://wallpapercave.com/w/wp10468545.jpg",
-    "https://wallpapercave.com/w/wp2510248.jpg",
-    "https://wallpapercave.com/w/wp4675248.jpg",
-    "https://wallpapercave.com/w/wp2397161.jpg",
-    "https://wallpapercave.com/w/wp4645342.jpg",
-    "https://wallpapercave.com/w/wp4645352.jpg",
-    "https://wallpapercave.com/w/wp9993140.jpg",
-    "https://wallpapercave.com/w/wp9993152.jpg",
-    "https://wallpapercave.com/w/wp4671788.jpg",
-    "https://wallpapercave.com/wp/wp4104529.jpg",
-    "https://wallpapercave.com/wp/wp10472490.jpg",
-    "https://wallpapercave.com/wp/wp9796957.jpg",
-    "https://wallpapercave.com/wp/wp3027130.png",
+			"https://wallpapercave.com/w/wp8808959.jpg",
+			"https://wallpapercave.com/w/wp10472453.jpg",
+    			"https://wallpapercave.com/w/wp10502274.jpg",
+    			"https://wallpapercave.com/w/wp7850224.jpg",
+    			"https://wallpapercave.com/w/wp10468512.jpg",
+    			"https://wallpapercave.com/w/wp9832405.jpg",
+    			"https://wallpapercave.com/w/wp10468545.jpg",
+    			"https://wallpapercave.com/w/wp2510248.jpg",
+    			"https://wallpapercave.com/w/wp4675248.jpg",
+    			"https://wallpapercave.com/w/wp2397161.jpg",
+    			"https://wallpapercave.com/w/wp4645342.jpg",
+    			"https://wallpapercave.com/w/wp4645352.jpg",
 		];
 		const selected = images[Math.floor(Math.random() * images.length)];
-		const text = `💖(uWu)💖\n\n`;
+		const text = `💖(here)💖\n\n`;
 			this.client.sendMessage( M.from, { url: selected }, MessageType.image, {
 				caption: `${text}`,
 			}

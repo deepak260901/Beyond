7247768ce6861a71c74837ee13021f1e25abf157
DEVILL-MASCOT <102029388+DEVILL-MASCOT@users.noreply.github.com>
2022-05-24 00:25:18 +0530

    Create wattpad.ts

---
+++ src/commands/Utils/wattpad.ts
@@ -0,0 +1,33 @@
+import { MessageType } from '@adiwajshing/baileys'
+import request from '../../lib/request'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+        command: "wattpad",
+	description: `search wattpad stories`,
+  	aliases: ["wartpad"],
+  	category: "Creations",
+	usage: `${client.config.prefix}wattpad`,
+	baseXp: 50,
+        })
+    }
+    // static count = 0
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        if (!joined) return void (await M.reply(`Give me your name, Baka!`))
+        const chitoge = joined.trim()
+        return void M.reply( await request.buffer(`https://api-xcoders.xyz/api/search/wattpad?query=${chitoge}&apikey=nMLptkNt9s`),
+        MessageType.text,
+                    undefined,
+                    undefined,
+                    `✨ Here you go.\n`,
+                    undefined
+                ).catch((reason: any) =>
+            M.reply(`✖ An error occurred. Please try again later. ${reason}`))
+    }
+}
